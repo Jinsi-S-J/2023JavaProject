869041b1460c5218170147c0c9796c57ba9a9539
Jinsi-S-J <121845984+Jinsi-S-J@users.noreply.github.com>
2023-01-09 17:57:15 +0530

    January 9 Class
    January 9 Class

--- src/Day01/Program01.java
+++ src/Day01/Program01.java
@@ -1,10 +1,31 @@
 package Day01;
 
 public class Program01 {
+	byte a=-128;
+	short b= 32767;
+	int c=1234567;
+	long d=67890898l;
+	boolean x=true;
+	char y='1';
+	float ab=0.1f;
+	double bc=0.1d;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("January 9 Class");
+		sum ();
+		Program01 abc= new Program01();
+		abc.Diff();
+		System.out.println(abc.c);
 
 	}
+	public static void sum () 
+	{
+		System.out.println("The sum of two integers");
+	}
+	public void Diff() 
+	{
+		System.out.println("The difference of two Integers");
+	}
+	}
 
-}
+
